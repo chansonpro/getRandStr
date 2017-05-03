0d4c1b8ea4a96b2b1e067dae82e6a6527d82d24f
Chanson <chansonpro@outlook.com>
2017-05-03 09:54:03 +0800

    get rand string

---
+++ getRandStr.php
@@ -0,0 +1,22 @@
+<?php
+function GetRandStr($len) 
+{ 
+    $chars = array( 
+        "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k",  
+        "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v",  
+        "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G",  
+        "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R",  
+        "S", "T", "U", "V", "W", "X", "Y", "Z", "0", "1", "2",  
+        "3", "4", "5", "6", "7", "8", "9" 
+    ); 
+    $charsLen = count($chars) - 1; 
+    shuffle($chars); //把数组中的元素按随机顺序重新排序：
+    $output = ""; 
+    for ($i=0; $i<$len; $i++) 
+    { 
+        $output .= $chars[mt_rand(0, $charsLen)]; 
+		//mt_rand() 使用 Mersenne Twister 算法返回随机整数。它可以产生随机数值的平均速度比 libc 提供的 rand() 快四倍。
+    }  
+    return $output;  
+} 
+echo GetRandStr(4);
